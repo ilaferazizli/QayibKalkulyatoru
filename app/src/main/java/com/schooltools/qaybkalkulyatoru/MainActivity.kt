3fe485c19df5d0c9e13d61dce4224a645a488828
ederson <ilaferezizli2@gmail.com>
2024-05-01 01:23:19 +0400

    Bottom Navigation

--- app/src/main/java/com/schooltools/qaybkalkulyatoru/MainActivity.kt
+++ app/src/main/java/com/schooltools/qaybkalkulyatoru/MainActivity.kt
@@ -2,10 +2,15 @@ package com.schooltools.qaybkalkulyatoru
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.schooltools.qaybkalkulyatoru.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bind:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
     }
 }
