6a507a458cbb32182291fbe4d887474eedddd865
ederson <ilaferezizli2@gmail.com>
2024-05-05 01:07:23 +0400

    Floating Action Button

--- app/src/main/java/com/schooltools/qaybkalkulyatoru/MainActivity.kt
+++ app/src/main/java/com/schooltools/qaybkalkulyatoru/MainActivity.kt
@@ -2,6 +2,11 @@ package com.schooltools.qaybkalkulyatoru
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.schooltools.qaybkalkulyatoru.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
-
+        val navHost=supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController= navHost.navController
+        bind.bottomNav.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/schooltools/qaybkalkulyatoru/SettingFragment.kt
+++ app/src/main/java/com/schooltools/qaybkalkulyatoru/SettingFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [camera.newInstance] factory method to
+ * Use the [SettingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class camera : Fragment() {
+class SettingFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class camera : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class camera : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment camera.
+         * @return A new instance of fragment settingFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            camera().apply {
+            SettingFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
